ecadaea38d7a1a6ecc764de98a05b8f7aebf91e5
Brian Simon <tarsipix@gmail.com>
2014-05-07 18:40:28 -0400

    add project id to pledge model and create has many and belongs to association

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -2,5 +2,6 @@ class Pledge < ActiveRecord::Base
 
     belongs_to :tier
     belongs_to :user
+    belongs_to :project
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507213048) do
+ActiveRecord::Schema.define(version: 20140507222209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140507213048) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "project_id"
   end
 
   create_table "projects", force: true do |t|
