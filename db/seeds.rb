eddcd97c77e05f08d999f153fc72ca41cd31daf0
Brian Simon <tarsipix@gmail.com>
2014-05-08 00:04:46 -0400

    added seed for user1@crowdfunder.com/12345 user2 and project, tier, pledges

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.create(        
+        email: "user1@crowdfunder.com",
+        password: "12345",
+        password_confirmation: "12345"
+)
+
+User.create(        
+        email: "user2@crowdfunder.com",
+        password: "12345",
+        password_confirmation: "12345"
+)
 
 Project.create(
 
@@ -14,10 +25,46 @@ Project.create(
         start_date: "2014-05-10",
         end_date: "2014-06-01",
         goal: 1000000,
-        raised: 0
+        raised: 0,
+        user_id: 1
 
+        )
+
+Tier.create(
+        project_id: 1,
+        amount: 1,
+        rewards: "No reward."
 )
 
+Tier.create(
+        project_id: 1,
+        amount: 10,
+        rewards: "You get a virtual hug!"
+)
+
+Tier.create(
+        project_id: 1,
+        amount: 100,
+        rewards: "You get a hug from a homeless person of your choosing!"
+)
+
+Tier.create(
+        project_id: 1,
+        amount: 2000,
+        rewards: "You get a limited edition hug from the homeless person you helped with lots of love :)"
+)
+
+Pledge.create(
+        tier_id: 1,
+        user_id: 1,
+        project_id: 1
+)
+
+Pledge.create(
+        tier_id: 2,
+        user_id: 1,
+        project_id: 1
+)
 
 Project.create(
 
@@ -26,11 +73,34 @@ Project.create(
         start_date: "2014-05-15",
         end_date: "2014-06-09",
         goal: 2000000,
-        raised: 0
+        raised: 0,
+        user_id: 2
 
+        )
+
+Tier.create(
+        project_id: 2,
+        amount: 1,
+        rewards: "No reward."
 )
 
+Tier.create(
+        project_id: 2,
+        amount: 15,
+        rewards: "You will receive a photo of a random cat."
+)
 
+Tier.create(
+        project_id: 2,
+        amount: 150,
+        rewards: "Receive a photo of a group of cats."
+)
+
+Tier.create(
+        project_id: 2,
+        amount: 5000,
+        rewards: "You will receive an authentic signed photo of the original grumpy cat!"
+)
 
 Project.create(
 
@@ -39,6 +109,25 @@ Project.create(
         start_date: "2014-05-21",
         end_date: "2014-06-21",
         goal: 3000000,
-        raised: 0
+        raised: 0,
+        user_id: 1
 
+        )
+
+Tier.create(
+        project_id: 3,
+        amount: 1,
+        rewards: "Racoons won't go through your garbage for one night only."
+)
+
+Tier.create(
+        project_id: 3,
+        amount: 100,
+        rewards: "Racoons will keep their offsprings away from your area."
+)
+
+Tier.create(
+        project_id: 3,
+        amount: 100000,
+        rewards: "Racoons will leave you alone FOR LIFE!"
 )
